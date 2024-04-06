1fb3d9dccea09b0c2b6ea6a1ecb636a9de422ae1
ncko <hello@ncko.me>
2024-04-06 16:55:20 -0700

    add stubs for linked_list

--- include/dsa/linked_list.h
+++ include/dsa/linked_list.h
@@ -5,10 +5,19 @@ typedef struct List {
 } List;
 
 List *List_create();
+
 void List_push(List *list, void *value);
 void *List_pop(List *list);
 void List_unshift(List *list, void *value);
 void *List_shift(List *list);
+
+List *List_copy(List *list);
+void List_join(List *list1, List *list2);
+List **List_split(List *list, int index);
+
+void List_reverse(List *list);
+void List_insert_sorted(List *list, void *value);
+
 void List_free(List *list);
 
 #endif
--- src/dsa/linked_list.c
+++ src/dsa/linked_list.c
@@ -3,8 +3,17 @@
 #include "dsa/linked_list.h"
 
 List *List_create() { return NULL; }
+
 void List_push(List *list, void *value) {}
 void *List_pop(List *list) { return NULL; }
 void List_unshift(List *list, void *value) {}
 void *List_shift(List *list) { return NULL; }
+
+List *List_copy(List *list) { return NULL; }
+void List_join(List *list1, List *list2) {}
+List **List_split(List *list, int index) { return NULL; }
+
+void List_reverse(List *list) {}
+void List_insert_sorted(List *list, void *value) {}
+
 void List_free(List *list) {}
